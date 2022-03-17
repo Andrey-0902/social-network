af91bc58d04304c6611bcfdca7fba2bad2e8b987
Andrey <Andrey-0902@yandex.ru>
2022-03-17 20:52:33 +0800

    Added-links-and-routes-to-other-components,-modified-styles-in-NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./Components/Header/Header";
 import NavBar from "./Components/Navbar/NavBar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import News from "./Components/News/News";
+import Music from "./Components/Music/Music";
+import Settings from "./Components/Settings/Settings";
 
 const App = () => {
     return (
@@ -13,8 +16,13 @@ const App = () => {
                 <Header/>
                 <NavBar/>
                 <div className="mainContent">
-                    <Route component={Dialogs}/>
-                    <Route component={Profile}/>
+                    <Routes>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
                 </div>
             </div>
         </BrowserRouter>
--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import classes from './NavBar.module.css';
+
 const NavBar = () => {
     return (
         <nav className={classes.mainNav}>
             <ul>
-                <li className={classes.item}><a href="/profile">Profile</a></li>
+                <li className={`${classes.item} ${classes.active}`}><a href="/profile">Profile</a></li>
                 <li className={classes.item}><a href="/dialogs">Messages</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Music</a></li>
-                <li><a href="#">Settings</a></li>
+                <li className={classes.item}><a href="/news">News</a></li>
+                <li className={classes.item}><a href="/music">Music</a></li>
+                <li className={classes.item}><a href="/settings">Settings</a></li>
             </ul>
         </nav>
     );
