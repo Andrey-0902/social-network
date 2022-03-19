84bdcca2904b9bb0b7873bd316968d43812844cb
Andrey <Andrey-0902@yandex.ru>
2022-03-19 13:17:24 +0800

    added-content-in-dialog.jsx-and-style

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import classes from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
-        </div>
+        <section className={classes.dialogsSection}>
+            <ul className={classes.dialogsList}>
+                <li className={classes.gialog + " " + classes.active}>Конь в пальто</li>
+                <li className={classes.gialog}>Мышка норушка</li>
+                <li className={classes.gialog}>Кот в сапогах</li>
+                <li className={classes.gialog}>Егоза</li>
+                <li className={classes.gialog}>Панда</li>
+            </ul>
+            <ul className={classes.messagesList}>
+                <li className={classes.messageItem}>Привет, как дела?</li>
+                <li className={classes.messageItem}>Учу React</li>
+                <li className={classes.messageItem}>Yo</li>
+            </ul>
+        </section>
     )
 }
 
